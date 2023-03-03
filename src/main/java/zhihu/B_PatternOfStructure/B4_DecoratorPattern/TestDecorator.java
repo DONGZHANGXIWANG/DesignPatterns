94cb54f283a32658abd4d4c4c6cde69bfdd1d1ca
S9644 <laiyup@4px.com>
2023-03-03 16:08:09 +0800

    装饰者模式

--- src/main/java/zhihu/B_PatternOfStructure/B4_DecoratorPattern/TestDecorator.java
+++ src/main/java/zhihu/B_PatternOfStructure/B4_DecoratorPattern/TestDecorator.java
@@ -7,6 +7,11 @@ import zhihu.B_PatternOfStructure.B4_DecoratorPattern.Origin.impl.OriginalCoffee
 
 /**
  * 装饰者模式
+ *
+ * 可以从客户端调用代码看出，装饰者模式的精髓在于动态的给对象增减功能。
+ *
+ * 当你你需要原味咖啡时那就生成原味咖啡的对象，而当你需要加奶咖啡时，仅仅需要将原味咖啡对象传递到加奶装饰者中去装饰一下就好了。如果你加了奶还想加糖，那就把加了奶的咖啡对象丢到加糖装饰者类中去装饰一下，一个先加奶后加糖的咖啡对象就出来了。
+ *
  */
 public class TestDecorator {
 
